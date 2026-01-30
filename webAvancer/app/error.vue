3cda7382dba1476f88e590c89666a4d990a70fed
jahaa69 <assunnaki@gmail.com>
2026-01-30 12:37:17 +0100

    fix

--- webAvancer/app/error.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="error-page">
-    <h1>Error {{ error.statusCode }}</h1>
-    <p>{{ error.message }}</p>
-    <nuxt-link to="/">Go back to Home</nuxt-link>
-  </div>
-</template>
